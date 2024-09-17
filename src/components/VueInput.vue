8a31cd5395156644ba610163ff05aff09be0e9fb
김진범 <lljrwq123@gmail.com>
2024-09-17 15:34:16 +0900

    Add: vue component from vue worktree

---
+++ src/components/VueInput.vue
@@ -0,0 +1,17 @@
+<template>
+  <input :value="value" name="vueInput" id="vueInput" @input="onInput">
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const value = ref('')
+const onInput = (e: Event) => {
+  value.value = (e.target as HTMLInputElement).value
+}
+
+</script>
+
+<style scoped>
+
+</style>
