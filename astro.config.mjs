386e1e75cd107683f1bb13332c7fab1b5340bd7d
김진범 <lljrwq123@gmail.com>
2024-09-17 19:43:39 +0900

    Add: SvelteInput component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-
 import react from '@astrojs/react';
-
 import lit from '@astrojs/lit';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), react(), lit()],
+  integrations: [vue(), react(), lit(), svelte()],
 });
---
+++ src/components/SvelteInput.svelte
@@ -0,0 +1,15 @@
+<script>
+  let value = '';
+
+  const handleInput = (e) => {
+    value = e.target.value + '!';
+  };
+</script>
+
+<input
+  id="SvelteInput"
+  name="SvelteInput"
+  type="text"
+  value="{value}"
+  on:input={handleInput}
+/>
